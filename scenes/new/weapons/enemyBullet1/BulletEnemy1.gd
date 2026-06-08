7c4b9cfa51144da099e31f33c724aa91dc41591e
ABHIRAM MS <72246739+abhiram-ms@users.noreply.github.com>
2026-06-08 12:51:14 +0530

    Remove old enemy bullet signal aliases for Godot 4 migration

--- scenes/new/weapons/enemyBullet1/BulletEnemy1.gd
+++ scenes/new/weapons/enemyBullet1/BulletEnemy1.gd
@@ -19,10 +19,6 @@ func _on_bullet_enemy_1_body_entered(body):
 			Global.camera.shake(100)
 		completeExplotion()
 
-# Compatibility for old scene signal connection names that came from Godot 3.
-func _on_BulletEnemy1_body_entered(body):
-	_on_bullet_enemy_1_body_entered(body)
-
 
 func explode():
 	var _particle = deathparticle.instantiate()
@@ -34,10 +30,6 @@ func explode():
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
-# Compatibility for old scene signal connection names that came from Godot 3.
-func _on_VisibilityNotifier2D_screen_exited():
-	_on_visible_on_screen_notifier_2d_screen_exited()
-
 func completeExplotion():
 	explode()
 	queue_free()
@@ -46,7 +38,3 @@ func completeExplotion():
 func _on_bullet_enemy_1_area_entered(area):
 	if area.is_in_group("playerBullet"):
 		completeExplotion()
-
-# Compatibility for old scene signal connection names that came from Godot 3.
-func _on_BulletEnemy1_area_entered(area):
-	_on_bullet_enemy_1_area_entered(area)
