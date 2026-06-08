6b9e126231fb3401a347d5c1747e9c9d70c55d6f
ABHIRAM MS <72246739+abhiram-ms@users.noreply.github.com>
2026-06-08 11:57:51 +0530

    Replace instance with instantiate in global script

--- scenes/GlobalScripts/global.gd
+++ scenes/GlobalScripts/global.gd
@@ -50,7 +50,7 @@ func _ready():
 
 
 func instance_node(node,location,parent):
-	var node_instance = node.instance()
+	var node_instance = node.instantiate()
 	parent.add_child(node_instance)
 	node_instance.global_position = location
 	return node_instance
