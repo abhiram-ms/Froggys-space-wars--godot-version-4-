b9376b1e4914fe04199387932ee0e64f7f71edfe
ABHIRAM MS <72246739+abhiram-ms@users.noreply.github.com>
2026-06-08 12:45:30 +0530

    Add Godot 3 signal compatibility aliases for enemy bullets

--- scenes/new/weapons/enemyBullet1/BulletEnemy1.gd
+++ scenes/new/weapons/enemyBullet1/BulletEnemy1.gd
@@ -3,7 +3,6 @@ extends Area2D
 var direction = Vector2.RIGHT
 var speed = 150
 
-
 @export var deathparticle : PackedScene
 @onready var enemy_fire = $enemy_fire
 @export var damage: int = 1
@@ -13,28 +12,41 @@ func _process(delta):
 	translate(direction * speed * delta)
 	
 
-func _on_BulletEnemy1_body_entered(body):
+func _on_bullet_enemy_1_body_entered(body):
 	if body == Global.player:
 		body.takeDamage(damage)
-		Global.camera.shake(100)
+		if Global.camera:
+			Global.camera.shake(100)
 		completeExplotion()
+
+# Compatibility for old scene signal connection names that came from Godot 3.
+func _on_BulletEnemy1_body_entered(body):
+	_on_bullet_enemy_1_body_entered(body)
 
 
 func explode():
-		var _particle = deathparticle.instantiate()
-		_particle.position = global_position
-		_particle.rotation =global_rotation 
-		_particle.emitting = true
-		get_parent().add_child(_particle)
+	var _particle = deathparticle.instantiate()
+	_particle.position = global_position
+	_particle.rotation = global_rotation
+	_particle.emitting = true
+	get_parent().add_child(_particle)
 
-func _on_VisibilityNotifier2D_screen_exited():
+func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+# Compatibility for old scene signal connection names that came from Godot 3.
+func _on_VisibilityNotifier2D_screen_exited():
+	_on_visible_on_screen_notifier_2d_screen_exited()
 
 func completeExplotion():
 	explode()
 	queue_free()
 
 
-func _on_BulletEnemy1_area_entered(area):
+func _on_bullet_enemy_1_area_entered(area):
 	if area.is_in_group("playerBullet"):
 		completeExplotion()
+
+# Compatibility for old scene signal connection names that came from Godot 3.
+func _on_BulletEnemy1_area_entered(area):
+	_on_bullet_enemy_1_area_entered(area)
