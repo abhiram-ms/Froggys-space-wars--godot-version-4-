b7482fb499a91f9676a8dc231635f0daaa98a4e2
ABHIRAM MS <72246739+abhiram-ms@users.noreply.github.com>
2026-06-08 12:13:29 +0530

    Make login work without Google Play Services

--- scenes/new/UI/LogIn/LogIn.gd
+++ scenes/new/UI/LogIn/LogIn.gd
@@ -1,87 +1,71 @@
 extends Control
 
-
 @onready var animation = $AnimationPlayer
 
-var GPGS;
+var GPGS = null
 
 func _ready():
 	if PlayService.GPGS:
-		PlayService.GPGS.connect("_on_sign_in_success", Callable(self, "_on_sign_in_success"))
-		PlayService.GPGS.connect("_on_sign_in_failed", Callable(self, "_on_sign_in_failed"))
-		
-		##game data save
-#		PlayService.GPGS.connect("_on_game_saved_success",self,"_on_game_saved_success")
-#		PlayService.GPGS.connect("_on_game_saved_fail",self,"_on_game_saved_fail")
-#		PlayService.GPGS.connect("_on_game_load_success",self,"_on_game_load_success")
-#		PlayService.GPGS.connect("_on_game_load_fail",self,"_on_game_load_fail")
-#		PlayService.GPGS.connect("_on_create_new_snapshot",self,"_on_create_new_snapshot")
-		
-	if PlayService.single:
-		OS.alert("Error in play games services")
+		if PlayService.GPGS.has_signal("_on_sign_in_success"):
+			PlayService.GPGS.connect("_on_sign_in_success", Callable(self, "_on_sign_in_success"))
+		if PlayService.GPGS.has_signal("_on_sign_in_failed"):
+			PlayService.GPGS.connect("_on_sign_in_failed", Callable(self, "_on_sign_in_failed"))
+	else:
+		print("Google Play Services unavailable. Login will continue with local save.")
+
 	animation.play("intro")
 
 
 func _on_LogIn_pressed():
-#	get_tree().change_scene("res://scenes/new/UI/MainMenu/scene1.tscn")
-	if PlayService.GPGS:
+	if PlayService.GPGS and PlayService.GPGS.has_method("signIn"):
 		PlayService.GPGS.signIn()
-#	else:
-#		get_tree().change_scene("res://scenes/new/UI/MainMenu/scene1.tscn")
-#	if GPGS:
-#		GPGS.signIn()
+	else:
+		_start_game_with_local_save()
+
+
+func _start_game_with_local_save() -> void:
+	SaveFile.load_data()
+	get_tree().change_scene_to_file("res://scenes/new/UI/MainMenu/scene1.tscn")
+
 
 func loadGame():
-	if PlayService.GPGS:
-		PlayService.GPGS.loadSnapshot("gamedata")
+	SaveFile.load_data()
+
 
 func _on_sign_in_success(userInformation):
-#	OS.alert("success"+ userInformation)
-	SaveFile.loadData()
+	SaveFile.load_data()
 	get_tree().change_scene_to_file("res://scenes/new/UI/MainMenu/scene1.tscn")
 	
 func _on_sign_in_failed(errorCode:int):
-	OS.alert("failed cant connect to sign in")
+	push_error("Google Play sign in failed. Continuing with local save.")
+	_start_game_with_local_save()
 
 
-
-##play games services callbacks from connect
+## Play Games Services callbacks kept for future GPGS compatibility.
 func _on_game_saved_success():
-	OS.alert("GAME SAVED SUCCESS")
+	print("GAME SAVED SUCCESS")
 
 func _on_game_saved_fail():
-	OS.alert("Game saved fail")
+	push_error("Game cloud save failed")
 
 func _on_game_load_success(data):
-	OS.alert("load success")
-	var test_json_conv = JSON.new()
-	test_json_conv.parse(data)
-	var testData = test_json_conv.get_data()
+	print("Cloud load success")
+	var testData = JSON.parse_string(data)
 	
 	if testData == null:
-		SaveFile.gamedata = SaveFile.initialData
-		SaveFile.saveData()
+		SaveFile.gamedata = SaveFile.initial_data.duplicate(true)
+		SaveFile.save_data(false)
 	else:
 		SaveFile.gamedata = testData
-		SaveFile.saveData()
-	
-	OS.alert(data + "===="+ SaveFile.gamedata)
+		SaveFile.save_data(false)
 
 func _on_game_load_fail():
-	OS.alert("Game data loading failed")
+	push_error("Game cloud data loading failed. Using local save.")
+	SaveFile.load_local_data()
 
 func _on_create_new_snapshot(name:String):
-	OS.alert("Create new snapshot %s"%name)
+	print("Create new snapshot %s" % name)
 
-##game save metheods:
+## Game save method kept for future GPGS compatibility.
 func save_game() -> void:
-	if PlayService.GPGS:
-		var data_to_save: Dictionary = {
-		"name": "John", 
-		"age": 22,
-		"height": 1.82,
-		"is_gamer": true
-		}
-		PlayService.GPGS.saveSnapshot("john", JSON.new().stringify(data_to_save), "DESCRIPTION")
-	else:
-		OS.alert("no play services aces to load data")
+	SaveFile.save_data()
